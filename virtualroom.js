9533b7d25f97c7282b8cac791a51541689afdb36
Vitaly Gashkov <vitalygashkov@vk.com>
2025-08-13 21:24:37 +0500

    refactor: move to ESM; update deps

--- virtualroom.js
+++ virtualroom.js
@@ -1,8 +1,6 @@
-'use strict';
+import { defineExtension } from 'azot';
 
-const { defineExtension } = require('azot');
-
-module.exports = defineExtension({
+export default defineExtension({
   fetchContentMetadata: async (url) => {
     const recordId = new URL(url).searchParams.get('recordId');
 
